f8a18425fe37e73452f3412f16b64b32526a3df8
Lam Le <lamduyle08@gmail.com>
2025-10-19 10:19:14 -0400

    w

--- IDE AND VCS Workflow Project2.cpp
+++ IDE AND VCS Workflow Project2.cpp
@@ -19,14 +19,14 @@ struct student {
 };
 // function getData reads student data from a file and returns an array of student structs
 // It also sets the number of students and tests via reference parameters.
-student* getData(ifstream& file, int& studentCnt, int& testsCnt);
+student* getData(ifstream& file, int& studentCount, int& testsCount);
 // function calcAverage calculates the average score and letter grade for each student
 // based on their test scores.
-void calcAverage(student students[], int studentCnt, int testsCnt);
+void calcAverage(student students[], int studentCount, int testsCount);
 // function printReport prints a formatted report of student names,
 // IDs, averages, and letter grades to the console.
 // It takes a constant array of student structs and the number of students as parameters.
-void printReport(const student students[], int studentCnt);
+void printReport(const student students[], int studentCount);
 // function getLetterGrade returns the letter grade corresponding to a given average score.
 // It takes a double average as a parameter and returns a char letter grade.
 char getLetterGrade(double avg);
@@ -48,32 +48,32 @@ int main() {
 	return 0;
 }
 
-student* getData(ifstream& file, int& studentCnt, int& testsCnt) {
-	file >> studentCnt >> testsCnt;
-	student* students = new student[studentCnt];
-	for (int i = 0; i < studentCnt; i++) {
+student* getData(ifstream& file, int& studentCount, int& testsCount) {
+	file >> studentCount >> testsCount;
+	student* students = new student[studentCount];
+	for (int i = 0; i < studentCount; i++) {
 		file >> students[i].name >> students[i].id;
-		students[i].scores = new double[testsCnt];
-		for (int j = 0; j < testsCnt; j++) {
+		students[i].scores = new double[testsCount];
+		for (int j = 0; j < testsCount; j++) {
 			file >> students[i].scores[j];
 		}
 	}
 	return students;
 }
-void calcAverage(student students[], int studentCnt, int testsCnt) {
-	for (int i = 0; i < studentCnt; i++) {
+void calcAverage(student students[], int studentCount, int testsCount) {
+	for (int i = 0; i < studentCount; i++) {
 		double total = 0;
-		for (int j = 0; j < testsCnt; j++) {
+		for (int j = 0; j < testsCount; j++) {
 			total += students[i].scores[j];
 		}
 
-		students[i].average = total / testsCnt;
+		students[i].average = total / testsCount;
 		students[i].lettergrade = getLetterGrade(students[i].average);
 	}
 }
-void printReport(const student students[], int studentCnt) {
+void printReport(const student students[], int studentCount) {
 	cout << left << setw(15) << "Name" << setw(10) << "ID" << setw(10) << "Average" << setw(10) << "Grade" << endl;
-	for (int i = 0; i < studentCnt; i++) {
+	for (int i = 0; i < studentCount; i++) {
 		cout << left << setw(15) << students[i].name << setw(10) << students[i].id
 			<< setw(10) << fixed << setprecision(1) << students[i].average
 			<< setw(10) << students[i].lettergrade << endl;
