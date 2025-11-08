3bdfc7da3c9fe0ae3da819cf473b9779c5369e0a
Lam Le <lamduyle08@gmail.com>
2025-11-07 22:37:52 -0500

    w

--- Date.h
+++ Date.h
@@ -12,5 +12,16 @@ private:
 public:
 	// Constructor
 	Date(int m = 1, int d = 1, int y = 1900);
-	
+	void setDate(int m, int d, int y);
+	int getDay() const;
+	int getMonth() const;
+	int getYear() const;
+	bool isLeapYear() const;
+	bool isLeapYear(int y) const;
+	int lastDayOfMonth() const;
+	int lastDayOfMonth(int m, int y) const;
+	string printDate() const;
+	string printDateLong() const;
+	string printOtherLong() const;
+};
 #endif // DATE_H
