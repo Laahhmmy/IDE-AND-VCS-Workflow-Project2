2056a3fb1a56578bc33a3296312262c62119d9fa
Lam Le <lamduyle08@gmail.com>
2025-11-07 22:36:03 -0500

    w

--- Date.h
+++ Date.h
@@ -1,1 +1,16 @@
-#pragm once
+#ifndef DATE_H
+#define DATE_H
+#include <string>
+using namespace std;
+
+class Date {
+private:
+	int day;
+	int month;
+	int year;
+	bool isvalidDate(int d, int m, int y);
+public:
+	// Constructor
+	Date(int m = 1, int d = 1, int y = 1900);
+	
+#endif // DATE_H
