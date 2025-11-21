cbe240f6d36ec56826b489729244ddc66e7af8f2
Lam Le <lamduyle08@gmail.com>
2025-11-20 21:26:11 -0500

    w

--- Date.h
+++ Date.h
@@ -1,5 +1,30 @@
-#pragma once
-class Date
-{
+#ifndef DATE_H // Header guard
+#define DATE_H 
+#include <string>
+using namespace std;
+
+class Date { // Date class definition
+private:
+	int day;
+	int month;
+	int year;
+	bool isvalidDate(int d, int m, int y);
+public:
+	// Constructor
+	Date(int m = 1, int d = 1, int y = 1900); // Default date is January 1, 1900
+	void setDate(int m, int d, int y); // Set date method
+	int getDay() const { return day; } // Getters
+	int getMonth() const { return month; } // Get month
+	int getYear() const { return year; }
+	bool isLeapYear() const; // Check if current year is leap year
+	bool isLeapYear(int y) const; // Check if given year is leap year
+	int lastDayOfMonth() const { // Get last day of current month
+		return lastDayOfMonth(month, year); // Call overloaded method
+	}
+	int lastDayOfMonth(int m, int y) const; // Get last day of given month and year
+	string printDate() const; // Print date in MM/DD/YYYY format
+	string printDateLong() const; // Print date in Month Day, Year format
+	string printOtherLong() const; // Print date in Day Month Year format
 };
+#endif // DATE_H
 
