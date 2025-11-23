3ff3f5e57b5bbd451c49544bf7446a583c9eb7d8
Lam Le <lamduyle08@gmail.com>
2025-11-23 11:12:20 -0500

    w

--- PoliceOfficer.h
+++ PoliceOfficer.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef POLICEOFFICER_H
+#define POLICEOFFICER_H
+#include <string>
+#include "ParkedCar.h"
+#include "ParkingMeter.h"
+#include "ParkingTicket.h"
+using namespace std;
+class PoliceOfficer {
+	private:
+	string name;
+	string badgeNumber;
+public:
+	PoliceOfficer(string nme, string badgeNum);
+
+	ParkingTicket inspectCar(const ParkedCar& car, const ParkingMeter& meter) const;
+
+
