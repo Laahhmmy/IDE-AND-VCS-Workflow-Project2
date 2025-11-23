321fdbf0f760d703bf6436a8eae42a9fae980fd4
Lam Le <lamduyle08@gmail.com>
2025-11-23 14:25:38 -0500

    w

--- IDE AND VCS Workflow Project2.cpp
+++ IDE AND VCS Workflow Project2.cpp
@@ -7,22 +7,40 @@
 #include "ParkingTicket.h"
 #include "PoliceOfficer.h"
 using namespace std;
-int main() {
-    ParkedCar car("Toyota", "Camry", "Red", "XYZ123", 30);
-    ParkingMeter meter(40);
-    PoliceOfficer officer("Officer John Doe", "5678");
+void runScenario(const ParkedCar& car, const ParkingMeter& meter, PoliceOfficer& officer);
 
-    ParkingTicket* ticket = officer.inspectCar(car, meter);
+  int main() {
+        PoliceOfficer officer("John Doe", "5678");
 
-    if (ticket) {
-        ticket->printTicket();
-        delete ticket;
-    }
-    else {
-        cout << "No violations found.\n";
-    }
+        ParkedCar car1("Toyota", "Camry", "Red", "XYZ123", 40);
+        ParkingMeter meter1(60);
 
-    return 0;
+        ParkedCar car2("Honda", "Accord", "Blue", "ABC987", 70);
+        ParkingMeter meter2(60);
+
+        ParkedCar car3("Ford", "Mustang", "Black", "LMN456", 190);
+        ParkingMeter meter3(60);
+
+        runScenario(car1, meter1, officer);
+        runScenario(car2, meter2, officer);
+        runScenario(car3, meter3, officer);
+
+        return 0;
 
 }
+  void runScenario(const ParkedCar& car, const ParkingMeter& meter, PoliceOfficer& officer) {
+      cout << "\n=========================\n";
+      cout << "Testing scenario:\n";
+      cout << car.getColor() << " " << car.getMake() << " " << car.getModel()
+          << " (" << car.getLicenseNumber() << ")" << endl;
 
+      ParkingTicket* ticket = officer.inspectCar(car, meter);
+
+      if (ticket) {
+          ticket->printTicket();
+          delete ticket;
+      }
+      else {
+          cout << "No violation found.\n";
+      }
+  }
