4e55cc6e92f7d141674a686a94ee884d3d2403e8
Lam Le <lamduyle08@gmail.com>
2025-11-23 14:06:02 -0500

    ww

--- IDE AND VCS Workflow Project2.cpp
+++ IDE AND VCS Workflow Project2.cpp
@@ -2,14 +2,27 @@
 //
 
 #include <iostream>
+#include "ParkedCar.h"
+#include "ParkingMeter.h"
+#include "ParkingTicket.h"
+#include "PoliceOfficer.h"
 using namespace std;
 int main() {
-	char first, middle, last;
-	first = 'L';
-	middle = 'D';
-	last = 'A';
-	cout << first << middle << last << endl;
-	return 0;
-}
+    ParkedCar car("Toyota", "Camry", "Red", "XYZ123", 30);
+    ParkingMeter meter(40);
+    PoliceOfficer officer("Officer John Doe", "5678");
 
+    ParkingTicket* ticket = officer.inspectCar(car, meter);
+
+    if (ticket) {
+        ticket->printTicket();
+        delete ticket;
+    }
+    else {
+        cout << "No violations found.\n";
+    }
+
+    return 0;
+
+}
 
