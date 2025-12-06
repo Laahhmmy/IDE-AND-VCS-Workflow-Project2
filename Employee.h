e806c4f253a7723ffeb02106acef3f4bf7b8c5b8
Lam Le <lamduyle08@gmail.com>
2025-12-06 11:07:31 -0500

    w

--- Employee.h
+++ Employee.h
@@ -1,5 +1,29 @@
-#pragma once
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+#include <string>
+using namespace std;
+
 class Employee
 {
+private:
+	string name;
+	string employeeNumber;
+	string hireDate;
+
+public:
+    Employee(string n = "", string num = "", string date = "");
+
+    // Accessors
+    string getName() const;
+    string getEmployeeNumber() const;
+    string getHireDate() const;
+
+    // Mutators
+    void setName(string n);
+    void setEmployeeNumber(string num);
+    void setHireDate(string date);
+
+    void printEmployee() const;
+
 };
 
