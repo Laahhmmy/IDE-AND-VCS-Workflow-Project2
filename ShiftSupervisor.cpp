f104140b405d42134a1c6baaf9c60a2957f753a9
Lam Le <lamduyle08@gmail.com>
2025-12-06 13:19:37 -0500

    w

--- ShiftSupervisor.cpp
+++ ShiftSupervisor.cpp
@@ -1,1 +1,20 @@
 #include "ShiftSupervisor.h"
+#include <iostream>
+using namespace std;
+
+ShiftSupervisor::ShiftSupervisor(string n, string numb, string date,
+    double salary, double bonus)
+    : Employee(n, numb, date), annualSalary(salary), productionBonus(bonus) {
+}
+
+double ShiftSupervisor::getAnnualSalary() const { return annualSalary; }
+double ShiftSupervisor::getProductionBonus() const { return productionBonus; }
+
+void ShiftSupervisor::setAnnualSalary(double s) { annualSalary = s; }
+void ShiftSupervisor::setProductionBonus(double b) { productionBonus = b; }
+
+void ShiftSupervisor::printShiftSupervisor() const {
+    printEmployee();
+    cout << "Annual Salary: $" << annualSalary << endl;
+    cout << "Production Bonus: $" << productionBonus << endl;
+}
--- ShiftSupervisor.h
+++ ShiftSupervisor.h
@@ -1,5 +1,24 @@
-#pragma once
-class ShiftSupervisor
+#ifndef SHIFTSUPERVISOR_H
+#define SHIFTSUPERVISOR_H
+#include "Employee.h"
+class ShiftSupervisor : public Employee
 {
-};
+private:
+	double annualSalary;
+	double productionBonus;
+public:
+    ShiftSupervisor(string n = "", string num = "", string date = "",
+        double salary = 0.0, double bonus = 0.0);
 
+    // Accessors
+    double getAnnualSalary() const;
+    double getProductionBonus() const;
+
+    // Mutators
+    void setAnnualSalary(double s);
+    void setProductionBonus(double b);
+
+    void printShiftSupervisor() const;
+
+};
+#endif
