82389be87416445ac2bb29dc9604e2f735a7137b
Lam Le <lamduyle08@gmail.com>
2025-12-07 12:13:36 -0500

    w

--- Rectangle.h
+++ Rectangle.h
@@ -1,5 +1,19 @@
-#pragma once
-class Rectangle
-{
-};
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include "BasicShape.h"
+class Rectangle : public BasicShape {
+private:
+		double width;
+		double length;
 
+public:
+	Rectangle(double l, double w, string n = "Rectangle");
+
+	double getLength() const { return length; }
+	double getWidth() const { return width; }
+
+	void calcArea() override;
+
+
+};
+#endif
