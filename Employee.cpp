5cb4c082a6bb720d2a43c98009bcb1762f1d7104
Lam Le <lamduyle08@gmail.com>
2025-12-14 14:24:42 -0500

    w

--- Employee.cpp
+++ Employee.cpp
@@ -1,1 +1,34 @@
 #include "Employee.h"
+#include <iostream>
+using namespace std;
+
+Employee::Employee(string n, string numb, string date)
+	: name(n), employeeNumber(numb), hireDate(date) {
+}
+
+string Employee::getName() const {
+	return name;
+}
+string Employee::getEmployeeNumber() const {
+	return employeeNumber;
+}
+string Employee::getHireDate() const {
+	return hireDate;
+}
+
+void Employee::setName(string n) {
+	name = n;
+}
+
+void Employee::setEmployeeNumber(string numb) {
+	employeeNumber = numb;
+}
+void Employee::setHireDate(string date) {
+	hireDate = date;
+}
+
+void Employee::printEmployee() const {
+	cout << "Name: " << name << endl;
+	cout << "Employee Number: " << employeeNumber << endl;
+	cout << "Hire Date: " << hireDate << endl;
+}
